b1be77e675a82353feb6c6f1a57e80d0d63acc90
Yura Kocyuba <yurakocyuba@yandex.ru>
2019-09-02 14:00:51 +0300

    fixed some mistakes in entrypoint.sh

--- chronicle/entrypoint.sh
+++ chronicle/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/opt/chronicle-receiver  --data-dir=${DATA_DIR} --config-dir=${DATA_DIR} --mode=${MODE} --host=${HOST} --port=${PORT} --exp-ws-host=${WS_HOST} --exp-ws-port=${WS_PORT} --exp-ws-bin-header=false --plugin=exp_ws_plugin --skip-block-events=${SKIP_BLOCK_EVENTS} skip-table-deltas=${SKIP_TABLE_DELTAS}
+/opt/chronicle-receiver  --data-dir=${DATA_DIR} --config-dir=${DATA_DIR} --mode=${MODE} --host=${HOST} --port=${PORT} --exp-ws-host=${WS_HOST} --exp-ws-port=${WS_PORT} --exp-ws-bin-header=false --plugin=exp_ws_plugin --skip-block-events=${SKIP_BLOCK_EVENTS} --skip-table-deltas=${SKIP_TABLE_DELTAS}
